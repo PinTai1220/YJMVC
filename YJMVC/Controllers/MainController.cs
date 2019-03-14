f7ab694b7b23dd5a9dc6b739ad39f4013452248a
2941451132@qq.com <2941451132@qq.com>
2019-03-14 21:14:16 +0800

    sa

---
+++ YJMVC/Controllers/MainController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace YJMVC.Controllers
+{
+    public class MainController : Controller
+    {
+        // GET: Main
+        public ActionResult MainIndex()
+        {
+            return View();
+        }
+        public ActionResult AboutIndex()
+        {
+            return View();
+        }
+        public ActionResult JieDaIndex()
+        {
+            return View();
+        }
+        public ActionResult TiaoIndex()
+        {
+            return View();
+        }
+        public ActionResult YinSiIndex()
+        {
+            return View();
+        }
+        public ActionResult ZiDaiIndex()
+        {
+            return View();
+        }
+    }
+}
