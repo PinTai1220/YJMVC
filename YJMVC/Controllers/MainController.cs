cd03e8efc73b6aa65f2e56ad2ee5b7bda8b0360f
2941451132@qq.com <2941451132@qq.com>
2019-03-15 16:47:18 +0800

    sa

--- YJMVC/Controllers/MainController.cs
+++ YJMVC/Controllers/MainController.cs
@@ -13,31 +13,43 @@ namespace YJMVC.Controllers
         /// </summary>
         /// <returns></returns>
         // GET: Main
+        //主页面
         public ActionResult MainIndex()
         {
             return View();
         }
+        //关于我们
         public ActionResult AboutIndex()
         {
             return View();
         }
+        //经常解答的问题
         public ActionResult JieDaIndex()
         {
             return View();
         }
+        //条款
         public ActionResult TiaoIndex()
         {
             return View();
         }
+        //隐私政策
         public ActionResult YinSiIndex()
         {
             return View();
         }
+        //咨询代理
         public ActionResult ZiDaiIndex()
         {
             return View();
         }
+        //服务
         public ActionResult FuWuIndex()
+        {
+            return View();
+        }
+        //联系我们
+        public ActionResult LianXiIndex()
         {
             return View();
         }
