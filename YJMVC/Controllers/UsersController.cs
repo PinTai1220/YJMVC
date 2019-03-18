47195afec2347d05a8da119e7d545eaf4b819f7f
WarlockJobs <998522+WarlockJobs@users.noreply.github.com>
2019-03-18 09:40:20 +0800

    注册 判断手机号是否已经注册过

--- YJMVC/Controllers/UsersController.cs
+++ YJMVC/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
@@ -7,6 +8,7 @@ using System.Linq;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Web;
 using System.Web.Mvc;
+using YJMVC.Helper;
 using YJMVC.Models;
 using YJMVC.MvcHelper;
 
@@ -35,8 +37,10 @@ namespace YJMVC.Controllers
             // 调用 Api的登录方法  或者调用返回所有值 然后 Lambda表达式判断
             // 返回MD5 加密格式的字符串
             string passworld = password.CalcMD5();
+            List<UsersModel> users = GetUsers();            // 调用方法获得所有的用户信息
+            UsersModel user = users.Where(c => c.User_Phone.Equals(email) && c.User_Pwd.Equals(password)).FirstOrDefault();
 
-            if (email.Equals("admin") && password.Equals("00000"))
+            if (user != null)
             {
                 Session["Account"] = email;
                 return 1;
@@ -56,12 +60,23 @@ namespace YJMVC.Controllers
             Session["Account"] = null;
             return 1;
         }
+        /// <summary>
+        /// 存储上传的图片
+        /// </summary>
         private static Bitmap headImg = null;
+        /// <summary>
+        /// 注册账户
+        /// </summary>
+        /// <param name="users"></param>
+        /// <returns></returns>
 
         public ActionResult Register(UsersModel users)
         {
             string path = Server.MapPath(@"/HeadImg/");
             string imgPath = $"{path}/{users.User_Phone}.png";
+
+            // 判断该图片是否存在
+
             headImg.Save(imgPath, ImageFormat.Png);
             users.User_PhotoUrl = imgPath;
 
@@ -69,7 +84,24 @@ namespace YJMVC.Controllers
             // 将密码进行MD5加密
             users.User_Pwd = users.User_Pwd.CalcMD5();
 
-            return Content("<script>alert('注册成功！');location.href='/Main/MainIndex'</script>");
+            users.User_Wx_Type = "1";
+            string json = JsonConvert.SerializeObject(users);
+
+            #region 调用Api进行注册
+            string jsonStr = HttpClientHelper.SendRequest("api/Users/Create", "post", json);
+            int result = JsonConvert.DeserializeObject<int>(jsonStr);
+
+            if (result > 0)
+            {
+                return Content("<script>alert('注册成功！');location.href='/Main/MainIndex'</script>");
+            }
+            else
+            {
+                return Content("<script>alert('注册失败！');location.href='/Main/MainIndex'</script>");
+            }
+
+            #endregion
+
         }
 
         /// <summary>
@@ -95,6 +127,37 @@ namespace YJMVC.Controllers
             Bitmap bmp = new Bitmap(memoryStream);
             headImg = bmp;
             return bmp;
+        }
+
+        /// <summary>
+        /// 获得所有的用户信息
+        /// </summary>
+        /// <returns></returns>
+        private List<UsersModel> GetUsers()
+        {
+            string jsonStr = HttpClientHelper.SendRequest("api/Users/Show", "Get");
+            List<UsersModel> result = JsonConvert.DeserializeObject<List<UsersModel>>(jsonStr);
+            return result;
+        }
+
+
+        /// <summary>
+        /// 焦点消失后 检测手机号是否存在
+        /// </summary>
+        /// <param name="phone"></param>
+        /// <returns></returns>
+        public int CheckPhoneExists(string phone)
+        {
+            List<UsersModel> users = GetUsers();
+            UsersModel user = users.Where(c => c.User_Phone.Equals(phone)).FirstOrDefault();
+            if (user != null)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
         }
     }
 }
